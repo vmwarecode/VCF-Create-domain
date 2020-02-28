de0149299b31bbb2fa1aee252cbcd088bdfed01e
VMware Developer Center <noreply@vmware.com>
2020-02-28 06:49:57 +0000

    Initial commit by dksudharshan

--- create_domain/create_domain.py
+++ create_domain/create_domain.py
@@ -11,21 +11,21 @@ def get_request(url,username,password):
     if(response.status_code == 200):
         data = json.loads(response.text)
     else:
-        print "Error reaching the server."
+        print ("Error reaching the server.")
         exit(1)
     return data
 
 def post_request(data,url,username,password):
     headers = {'Content-Type': 'application/json'}
-    print url
-    print data
+    print (url)
+    print (data)
     response = requests.post(url, headers=headers, json=data, auth=(username, password))
     if(response.status_code == 200 or response.status_code == 202):
         data = json.loads(response.text)
     else:
-        print "Error reaching the server."
+        print ("Error reaching the server.")
         response = json.loads(response.text)
-        print json.dumps(response,indent=4, sort_keys=True)
+        print (json.dumps(response,indent=4, sort_keys=True))
         exit(1)
     return data
 
@@ -36,10 +36,10 @@ def poll_on_id(url,username,password):
     while(status == 'IN_PROGRESS'):
         c = c+1
         n = c%6
-        print "\rOperation in progress","."*n," "*(5-n),
+        print ("\rOperation in progress" + "."*n + " "*(5-n),end = "")
         status = get_request(url,username,password)['status']
         time.sleep(5)
-    print ""
+    print ("")
     return status
 
 
@@ -47,26 +47,25 @@ def create_workload_domain(hostname,username,password):
     data = read_input()
     url =  hostname+'/v1/domains/validations/creations'
     response = post_request(data,url,username,password)
-    request_id = response['id']
-    print "Validating the input...."
+    print ('Validating the input....')
     if(response['executionStatus'] == 'COMPLETED' and response['resultStatus'] == 'SUCCEEDED'):
-        print 'Validation Succeeded.'
+        print ('Validation Succeeded.')
     else:
-        print 'Validation failed.'
-        print json.dumps(response,indent=4, sort_keys=True)
+        print ('Validation failed.')
+        print (json.dumps(response,indent=4, sort_keys=True))
         exit(0)
     url = hostname + '/v1/domains'
     response = post_request(data,url,username,password)
-    print 'Creating Domain...'
+    print ('Creating Domain...')
     task_id = response['id']
     url = hostname+'/v1/tasks/'+task_id
-    result = poll_on_id(url,username,password,True)
+    result = poll_on_id(url,username,password)
     if(result == 'SUCCESSFUL'):
-        print 'Successfully created Domain.'
+        print ('Successfully created Domain.')
     else:
-        print 'Domain Creation failed.'
-        print json.dumps(response,indent=4, sort_keys=True)
-        print result
+        print ('Domain Creation failed.')
+        print (json.dumps(response,indent=4, sort_keys=True))
+        print (result)
 
 
 def read_input():
@@ -78,7 +77,7 @@ def get_help():
     help_description = '''\n\t\t----Create Domain----
     Usage:
     python create_domain.py <hostname> <username> <password>\n Refer to documentation for more detais\n'''
-    print help_description
+    print (help_description)
 
 
 def action_performer():
@@ -88,7 +87,7 @@ def action_performer():
         return
     hostname = 'https://'+arguments[1]
     username = arguments[2]
-    passwsord = arguments[3]
+    password = arguments[3]
     create_workload_domain(hostname,username,password)
     return
 
